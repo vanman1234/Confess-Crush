c93c8010d2d85981714d67c14a9113619d0502aa
vanman1234 <91955412+vanman1234@users.noreply.github.com>
2021-10-05 18:49:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Mẫn ❤ Bối",
+    introTitle: 'Bùi Quỳnh Hương  ❤',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
